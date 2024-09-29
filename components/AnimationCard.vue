3fe81c34c3db0fa25b9f1f510e98bc95a53812b6
Kowenjko <ortoswt@gmail.com>
2024-09-29 13:17:17 +0300

    create component AnimationCard

---
+++ components/AnimationCard.vue
@@ -0,0 +1,52 @@
+<script lang="ts" setup></script>
+
+<template>
+	<div class="card">Click here</div>
+</template>
+
+<style lang="css" scoped>
+.card {
+	cursor: pointer;
+	border-radius: 10px;
+	background-color: rgb(25, 25, 90);
+
+	padding: 10px 30px;
+	color: white;
+	position: relative;
+}
+@property --angle {
+	syntax: '<angle>';
+	initial-value: 0deg;
+	inherits: false;
+}
+
+.card::after,
+.card::before {
+	--angle: 10deg;
+	content: '';
+	position: absolute;
+	height: calc(100% + 3px);
+	width: calc(100% + 3px);
+	border-radius: 10px;
+
+	background-color: red;
+	top: 50%;
+	left: 50%;
+	translate: -50% -50%;
+	z-index: -1;
+	background-image: conic-gradient(from var(--angle), #ff4545, #00ff99, #006aff, #ff0095, #ff4545);
+	animation: 3s spin linear infinite;
+}
+.card::before {
+	filter: blur(1.5rem);
+	opacity: 0.55;
+}
+@keyframes spin {
+	from {
+		--angle: 0deg;
+	}
+	to {
+		--angle: 360deg;
+	}
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
 const route = useRoute()
+
+useHead({
+	title: 'New Trello',
+})
 </script>
 
 <template>
-	<div class="flex flex-col text-center justify-center h-screen">
+	<div class="flex flex-col text-center justify-center items-center h-screen">
 		<h1>Nuxt Routing set up successfully!</h1>
-		<p>Current route: {{ route.name }}</p>
+		<p class="mb-5">Current route: {{ route.name }}</p>
+		<AnimationCard />
 	</div>
 </template>
